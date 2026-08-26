dcf9052dde0d55c61ea46dcfc2b6c45594c713fd
th14go12345ferreira-del <th14go12345ferreira@gmail.com>
2026-08-26 14:35:07 -0300

    Corrigir Java e Kotlin para versão 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,14 @@ android {
 
     androidResources {
         noCompress += "bin"
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
+}
     }
 
     packaging {
